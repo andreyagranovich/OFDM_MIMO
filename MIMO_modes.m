c7566b61e10932aec61f9efa556e7413a4d6efe3
Andrey Agranovich <andreyagranovich@gmail.com>
2020-01-07 21:32:45 +0200

    WIP: creating signal

--- MIMO_modes.m
+++ MIMO_modes.m
@@ -1,14 +1,28 @@
-function [ output_args ] = MIMO_modes( input_args )
+function [] = MIMO_modes( )
 %MIMO_MODES Summary of this function goes here
 %   Detailed explanation goes here
+clear all
+close all
+% sps
+SPS = 8;
+N = 10;%00; % number of symbols
+% pulse shape - RC
+RC_beta = 0.9;
+RC_span = 4;
 
 % QPSK Modulation
 M = (1:4);
 qpsk_symbols = exp(-1j.*(M*pi./2 + pi/4));
 
 % random signal
-N = 1000; % number of symbols
-x = randi([1, length(M)], 1, N);
+bit_x = randi([1, length(M)], 1, N)';
+bit_x_padded = zeros(N, 8);
+bit_x_padded(:,1) = qpsk_symbols(bit_x);
+bit_x_padded = reshape(bit_x_padded', 1, N*SPS);
+
+RC_window = rcosdesign(RC_beta, RC_span, SPS);
+
+x = qpsk_symbols(bit_x)
 
 end
 
